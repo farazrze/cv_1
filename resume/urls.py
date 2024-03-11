7eea8d3315d437539084357d7e37f0beb5b6fd54
Faraz <farazrze18@gmail.com>
2024-03-11 21:09:46 +0330

    static

--- resume/urls.py
+++ resume/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from .view import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('information.urls'))
+    path('information',include('information.urls')),
+    path('test',test),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
